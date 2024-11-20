c802e982452c77602c61bcde85908cbb804a8732
Fabio Marques <fabiojuniomarquesdamasio@gmail.com>
2024-11-19 21:49:09 -0300

    mais array

--- JS/ATT-Array2/att-11/script.js
+++ JS/ATT-Array2/att-11/script.js
@@ -1,56 +1,69 @@
-/*11- Você está desenvolvendo um aplicativo de agenda de contatos em JS. Crie um programa que permita ao usuário adicionar, editar, excluir e visualizar contatos em sua agenda. Os contatos devem ser armazenados em vetores que incluem nome, número de telefone e email.*/
-
-let nome = new Array();
-let numero = [];
-let email = new Array();
-let opcao = 0, pesquisa;
+/*11- Você está desenvolvendo um aplicativo de agenda de contatos em JS. Crie um programa que permita ao usuário adicionar, editar, excluir e visualizar contatos em sua agenda. Os contatos devem ser armazenados em vetores que incluem nome, número de telefone e email.
+*/
+let nome = [], numero = [], email = [], opcao = 0 , pesquisa;
 
 do {
-    opcao = Number(prompt(`1- Adicionar  2- Editar  3- Excluir  4- Visualizar  0- Sair`));
-
-    switch (opcao) {
+    opcao = Number(prompt(`1- Adicionar\n2- Editar\n3- Excluir\n4- Pesquisar\n0- Sair`));
+    switch (opcao){
+        case 0:
+            console.log(`SAIR`);
+            break;
         case 1:
-            nome.push = prompt("Digite o nome do contato:");
-            numero.push = Number(prompt("Digite o número de telefone do contato:"));
-            email.push = prompt("Digite o e-mail do contato:");
+            nome.push(prompt(`Digite um nome`));
+            numero.push(Number(prompt("Digite o telefone")));
+            email.push(prompt(`Digite o email`));
             break;
 
         case 2:
-            pesquisa = prompt (`Digite o contato a ser editado:`);
-            for (let i = 0; i < nome.length; i++){
-                if (nome[i] === pesquisa){
-                    nome[i] = prompt("Digite o novo nome do contato:");
-                    numero[i] = Number(prompt("Digite o novo número de telefone do contato:"));
-                    email[i] = prompt("Digite o novo e-mail do contato:");
-                }
-            }
-            break;
-            
-        case 3:
-            for (let i = 0; i < nome.length;i++){
+            pesquisa = prompt(`Digite o nome do contato para editar`);
+            let editado = false;
+            for (let contador = 0; contador < nome.length; contador++){
                 if (pesquisa == nome[contador]){
-                    delete nome[contador];
-                    delete numero[contador];
-                    delete email[contador];
+                    nome[contador] = prompt(`Digite o novo nome`);
+                    numero[contador] = Number(prompt("Digite o novo telefone"));
+                    email[contador] = prompt(`Digite o novo email`);
+                    console.log("Contato editado com sucesso!");
+                    editado = true;
+                    break;
+                }
+            }
+            if (!editado) {
+                console.log("Contato não encontrado.");
+            }
+            break;
+
+        case 3:
+            pesquisa = prompt(`Digite o nome do contato para excluir`);
+            let excluido = false;
+            for (let contador = 0; contador < nome.length; contador++){
+                if (pesquisa == nome[contador]){
+                    nome.splice(contador, 1);
+                    numero.splice(contador, 1);
+                    email.splice(contador, 1);
+                    console.log("Contato excluído com sucesso!");
+                    excluido = true;
+                    break;
+                }
+            }
+            if (!excluido) {
+                console.log("Contato não encontrado.");
+            }
+            break;
+
+        case 4:
+            if (nome.length === 0) {
+                console.log("Nenhum contato cadastrado.");
+            } else {
+                for (let contador = 0; contador < nome.length; contador++){
+                    console.log(`Nome: ${nome[contador]}`);
+                    console.log(`Telefone: ${numero[contador]}`);
+                    console.log(`Email: ${email[contador]}`);
                 }
             }
             break;
-            
-        case 4:
-            console.log("Contatos cadastrados:");
-            for (let i = 0; i < nome.length; i++){
-                console.log(nome[contador]);
-                console.log(numero[contador]);
-                console.log(email[contador]);
-            }
-            break;
-            default:
-                console.log("Opção inválida!");
-            break;
 
+        default:
+            console.log(`Opção inválida`);
+            break;
     }
-
-} while (opcao !== 0);
-
-
-
+} while (opcao != 0);
